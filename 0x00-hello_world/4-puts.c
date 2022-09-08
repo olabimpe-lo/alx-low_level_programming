bf629aba0e281edf1d7304a9662ac56c9f3c801f
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-08 11:26:40 -0700

    Task4-a script that prints an exact statements followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * function main begins program execution
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	puts( "programming is like building a multilingual puzzle\n" );
+	return (0);
+
+}

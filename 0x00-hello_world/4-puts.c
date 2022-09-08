ddba1043451f23cae16a8c35dd60346e57d25afb
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-08 12:01:24 -0700

    Task4-A new correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
- * function main begins program execution
+ * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
 {
 
-	puts( "programming is like building a multilingual puzzle\n" );
+	puts("programming is like building a multilingual puzzle\n");
 	return (0);
 
 }

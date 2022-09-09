d9c79c28537e177902a8417b82506c886dacce70
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-09 10:37:40 -0700

    removed scanf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,13 +20,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
-
-	scanf("%d", &n);
 	if ( n > 0)
-		printf("is positive\n", n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("is zero\n", n);
+		printf("%d is zero\n", n);
 	else (n < 0)
-		printf("is negative\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }

399c997369b54635eba9cb8eac7f9f363a5b0a37
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-11 21:34:59 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,9 +5,9 @@
 /**
  * main - print last digit in random number
  *
- * Decription: print last digit in random number
+ * Description: print last digit in random number
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -19,9 +19,9 @@ int main(void)
 	last = n % 10;
 	if (last > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	else if (last == 0)
+	else if last == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last);
-	else if (last < 6 && last != 0)
+	else if (last < 6 && last != o)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 
 	return (0);

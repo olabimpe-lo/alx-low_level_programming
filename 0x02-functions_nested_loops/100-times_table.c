01cb5029959d9f791e09d0010588fe42e9f15dcf
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-14 15:33:23 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the times table from 0
+ * @n: it is an integer
+ *
+ * Return: n
+ */
+
+void print_times_table(int n)
+{
+	int row, column, product;
+	if (n > 0) && (n < 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+
+			for (column = 0; row <=; row++)
+			{
+				
+				product = row * column;
+
+				if (column == 0)
+					_putchar('0');
+				else if (product < 10)
+				{	
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+

c867ec9e26329c85fceba32e4374d4680080ca00
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-14 01:44:43 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value of an integer
+ *
+ * n - is an integer
+ *
+ * Return: 0
+ */
+
+int _abs(int n);
+{
+	if (n > 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{ 
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}

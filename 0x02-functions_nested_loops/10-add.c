f2e08203d7ffde53569371d185b2602d2f10a922
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-14 07:24:06 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * m: The first integer
+ * n: The second integer
+ *
+ * Return: sum of integers
+ */
+
+int add(int m, int n)
+{
+	return (m + n);
+}

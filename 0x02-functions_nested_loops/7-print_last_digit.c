f749ac3d9e7d334db68176a9c55106a0253aed20
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-14 03:05:53 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print last digit of a number
+ *
+ * n: it is a number
+ *
+ * Return: integer
+ */
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	if (n < 0)
+		last = last * -1;
+
+	_putchar(last + '0');
+	return (last);
+}

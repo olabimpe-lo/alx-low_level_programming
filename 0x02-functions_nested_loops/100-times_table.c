a2c1aa0d32aecbbd1edd7e3de691b81ac2b497fd
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-14 15:40:06 -0700

    new correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,12 @@
 void print_times_table(int n)
 {
 	int row, column, product;
-	if (n > 0) && (n < 15)
+	if (n >= 0) && (n < 15)
 	{
 		for (row = 0; row <= n; row++)
 		{
 
-			for (column = 0; row <=; row++)
+			for (column = 0; column <= n; column++)
 			{
 				
 				product = row * column;

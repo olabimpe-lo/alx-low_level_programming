108d49258f230890d22f62e8813e8f4f56f9a6f2
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-15 16:02:57 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,7 @@
 
 int main(void)
 {
-
 	        int i;
-
 
 	        printf("Infinite loop incoming :(\n");
 
@@ -28,6 +26,5 @@ int main(void)
 		 printf("Infinite loop avoided! \\o/\n");
 
 		return (0);
-
 }
 

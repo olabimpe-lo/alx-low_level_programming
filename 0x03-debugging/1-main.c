bd48b511f01adead7d449b51461de99b41142da0
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-15 17:14:24 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  *
  * * main - causes an infinite loop
@@ -9,21 +8,19 @@
  *
  * */
 
-
-
 int main(void)
 {
 	       /* int i;*/
-
 	        printf("Infinite loop incoming :(\n");
 
 		/** i = 0;
-	        * 	 while (i < 10)
+		* 
+	        * while (i < 10)
 		* {
 		*	putchar(i);	
 		* }
 		*/
-		 printf("Infinite loop avoided! \\o/\n");
+		printf("Infinite loop avoided! \\o/\n");
 
 		return (0);
 }

78d7eda6f9770e1370656d7e8c74f8d4608f85b3
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-15 19:07:35 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,4 @@
-
-#include "main.h"
-
-#include <stdio.h>
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,3 +9,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);

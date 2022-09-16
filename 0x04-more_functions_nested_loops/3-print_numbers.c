b006f93e582cf1a07f9b37eae66b72ff8948fae5
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-15 19:33:25 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * @m - number to be printed
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	for (m = '0'; m <= '9'; m++)
+	{
+		_putchar(m);
+
+	}
+	_putchar('\n');
+}

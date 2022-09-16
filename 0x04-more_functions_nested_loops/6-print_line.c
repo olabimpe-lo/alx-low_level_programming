14aeac671bf107e08676c9a0a1efdb80a4cf3d75
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-16 01:45:47 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times _ should be printed
+ * 
+ * Return: n
+ */
+
+void print_line(int n)
+{
+	int n = 0;
+
+	while (line < n)
+	{
+		_putchar('_');
+		line++
+	}
+	_putchar('\n');
+}

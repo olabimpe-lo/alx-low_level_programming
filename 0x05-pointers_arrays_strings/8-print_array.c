c7bb613ac48a514e734f68611270d43d8df942a8
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-19 20:14:07 -0700

    A function that prints elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_array - print element of array of integers
+ * @a : represents array
+ * @n : represents integer
+ * Return : void
+ * */
+
+void print_array(int *a, int n)
+{
+	int m;
+	
+	if (n > 0)
+	{
+		for (m = 0; n > m; m++)
+		{
+			if(m != n -1)
+			{
+				printf("%d, ", a[m]);
+			}
+			else
+			{
+				printf("%d", a[m]);
+			}
+		}
+		printf("\n");
+	}
+}

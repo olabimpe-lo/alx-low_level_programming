0940526ec994fe82fcdbc7e771e85ed4864eceea
Tosin Lasisi <Oluwatosin Lasisi>
2022-09-19 19:22:41 -0700

    A function that prints reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: s is a string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int j, m, n;
+	char First, Last;
+
+	j = 0;
+	while (s[j] != '\0')
+	{
+		i++;
+	}
+	m = j - 1;
+	n = m / 2;
+	while (n >= 0)
+	{
+		He = s[m - n];
+		She = s[n];
+		s[n] = He;
+		s[m - n] = She;
+		n--;
+	}
+}
